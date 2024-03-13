ccbd712dd5fa80d639f710c0ed3daa0f7744fa0f
madsler <81021461+madsler@users.noreply.github.com>
2024-03-13 15:51:57 +0100

    Update kubeinvadersinstall.sh

--- kubeinvadersinstall.sh
+++ kubeinvadersinstall.sh
@@ -2,7 +2,6 @@
 #pre: DNS entry for ingress to kubeinvaders.io
 # 
 
-
 #!/bin/bash
 # install Helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | DESIRED_VERSION=v3.13.3 bash
@@ -11,21 +10,13 @@ curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | DESIR
 helm repo add kubeinvaders https://lucky-sideburn.github.io/helm-charts/
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 
-# Set KUBECONFIG
-#export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-
 # Helm Repo Update
 helm repo update
 
-#Prereqs f k3s
-#export KUBECONFIG=~/.kube/config
-#sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config && chown $USER ~/.kube/config && chmod 600 ~/.kube/config && export KUBECONFIG=~/.kube/config
-#mkdir ~/.kube 2> /dev/null
-#sudo k3s kubectl config view --raw > "$KUBECONFIG"
-#chmod 600 "$KUBECONFIG"
-
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/server/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # install k9s
 #curl -sS https://webinstall.dev/k9s | bash
@@ -36,7 +27,6 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable traefik" sh -s -
 mkdir ~/.kube
 sudo k3s kubectl config view --raw | tee ~/.kube/config
 chmod 600 ~/.kube/config
-#export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 # Create Namespaces
 sudo kubectl create ns kubeinvaders
@@ -81,7 +71,7 @@ spec:
 EOF
 
 # Apply Ingress-Nginx Configuration
-kubectl create -f /tmp/ingress-nginx.yaml
+sudo kubectl create -f /tmp/ingress-nginx.yaml
 
 # Install KubeInvaders Helm Chart
 helm install kubeinvaders --set-string config.target_namespace="namespace1\,namespace2" \
@@ -111,8 +101,8 @@ spec:
 EOF
 
 # Apply Nginx Deployment in namespace1
-kubectl apply -f deployment.yaml -n namespace1
-kubectl apply -f deployment.yaml -n namespace2
+sudo kubectl apply -f deployment.yaml -n namespace1
+sudo kubectl apply -f deployment.yaml -n namespace2
 
 # Scale Nginx Deployment in namespace1
 #kubectl scale deployment.apps/nginx-deployment --replicas=20 -n namespace1
